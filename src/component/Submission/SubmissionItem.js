2f776d700cf395c2f8f5076b28c3b5e2921f339e
jose.chambule <engchambule.@gmail.com>
2023-03-27 18:09:49 +0200

    add css on table

--- src/component/Submission/SubmissionItem.js
+++ src/component/Submission/SubmissionItem.js
@@ -6,53 +6,63 @@ function SubmissionItem(props) {
   const submissionCtx = useContext(SubmissionContext);
 
   return (
-    <div className={classes.sub}>
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Ano</th>
-            <th>Periodo</th>
-            <th>Parceiro</th>
-            <th>Ficheiro</th>
-            <th>CreadoPor</th>
-            <th>Ação</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.dados.map((val) => {
-            return (
-              <tr key={val.id}>
-                <td>{val.id}</td>
-                <td>{val.year}</td>
-                <td>{val.quarter}</td>
-                <td>{val.partner}</td>
-                <td>{val.fileName}</td>
-                <td>{val.createdBy}</td>
-                <td>
-                  <button
-                    className={classes.edit}
-                    onClick={() => {
-                      submissionCtx.addSubmission({
-                        id: val.id,
-                        ano: val.year,
-                        periodo: val.quarter,
-                        parceiro: val.partner,
-                        ficheiro: val.fileName,
-                        creadoPor: val.createdBy,
-                        password: val.password,
-                      });
-                      props.onClickEditar();
-                    }}
-                  >
-                    Editar
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div className={classes.container}>
+      <div className={classes.wraptable}>
+        <div className={classes.contenthead}>
+          <div className={classes.cabecalho}>
+            <table>
+              <thead>
+                <tr className={classes.rowhead}>
+                  <th>Id</th>
+                  <th>Ano</th>
+                  <th>Periodo</th>
+                  <th>Parceiro</th>
+                  <th>Ficheiro</th>
+                  <th>CreadoPor</th>
+                  <th>Ação</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+          <div className={classes.corpo}>
+            <table>
+              <tbody>
+                {props.dados.map((val) => {
+                  return (
+                    <tr key={val.id}>
+                      <td>{val.id}</td>
+                      <td>{val.year}</td>
+                      <td>{val.quarter}</td>
+                      <td>{val.partner}</td>
+                      <td>{val.fileName}</td>
+                      <td>{val.createdBy}</td>
+                      <td>
+                        <button
+                          className={classes.edit}
+                          onClick={() => {
+                            submissionCtx.addSubmission({
+                              id: val.id,
+                              ano: val.year,
+                              periodo: val.quarter,
+                              parceiro: val.partner,
+                              ficheiro: val.fileName,
+                              creadoPor: val.createdBy,
+                              password: val.password,
+                            });
+                            props.onClickEditar();
+                          }}
+                        >
+                          Editar
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
